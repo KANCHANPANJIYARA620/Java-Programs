bf281d2dd3c0bcb38a016e46ce024f81d34db0d8
Kanchan <kanchankrijps@gmail.com>
2025-11-15 12:56:08 +0530

    Copy Element A to B

--- learn-array/copying.java
+++ learn-array/copying.java
@@ -3,12 +3,12 @@ public class copying {
         int A[]= {2,4,5,7,9,19,20};
         int B[]= new  int[7];
         int n=7;
-        for(int i=0;i<n;i++){
-            B[i]=A[i];
+        for(int i=0 , j=0;i<n;i++,j++){
+            B[j]=A[i];
         }
         System.out.println("Element of B is :");
-        for(int i=0;i<n;i++){
-            System.out.println(B[i]);
+        for(int j=0;j<n;j++){
+            System.out.println(B[j]);
         }
     }
 }
